c52a7698dbfcb8c9690b47392777b735fe1d0c1a
ebertm777 <eber.tmacedo@gmail.com>
2020-07-29 22:26:29 -0300

    adicionado forms e useState

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,22 +1,48 @@
-import React from 'react';
-import PageDefault from '../../../components/PageDefault';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/PageDefault';
 
-function categoriaRegistro() {
+
+function CategoriaRegistro() {
+    const [categorias, setCategorias] = useState(['Teste']);
+    const [nomeDaCategoria, setNomeDaCategoria] = useState('nova categoria');
+
     return (
         <PageDefault>
-            <h1>Registro de Categoria.</h1>
-            <form>
+            <h1>Registro de Categoria:{nomeDaCategoria}</h1>
+
+            <form onSubmit={function handleSubmit(infoDoEvento) {
+                infoDoEvento.preventDefault();
+                setCategorias([
+                    ...categorias,
+                    nomeDaCategoria
+                ]);
+
+            }}>
                 <label>
                     Nome da Categoria:
                     <input
                         type="text"
+                        value={nomeDaCategoria}
+                        onChange={function errorHandler(infoDoEvento) {
+                            setNomeDaCategoria(infoDoEvento.target.value);
+                        }}
                     />
                 </label>
                 <button>
                     Registrar
                 </button>
             </form>
+
+            <ul>
+                {categorias.map((categoria) => {
+                    return (
+                        <li key={categoria}>
+                            {categoria}
+                        </li>
+                    )
+                })}
+            </ul>
 
 
             <Link to="/">
@@ -25,4 +51,4 @@ function categoriaRegistro() {
         </PageDefault>
     )
 }
-export default categoriaRegistro;
+export default CategoriaRegistro;
