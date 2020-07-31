dd36778c0fdf6b58ba5e97b366c9905ca44a7d39
ebertm777 <eber.tmacedo@gmail.com>
2020-07-31 20:15:24 -0300

    form update

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,41 +4,84 @@ import PageDefault from '../../../components/PageDefault';
 
 
 function CategoriaRegistro() {
-    const [categorias, setCategorias] = useState(['Teste']);
-    const [nomeDaCategoria, setNomeDaCategoria] = useState('nova categoria');
+    const valorInicial = {
+        nome: '',
+        descrição: '',
+        cor: '',
+    }
+    const [categorias, setCategorias] = useState([]);
+    const [values, setValues] = useState(valorInicial);
+
+    function setValue(chave, valor) {
+        setValues({
+            ...values,
+            [chave]: valor,
+        })
+    }
+
+    function errorHandler(infoDoEvento) {
+        setValue(
+            infoDoEvento.target.getAttribute('name'),
+            infoDoEvento.target.value
+        );
+    }
 
     return (
         <PageDefault>
-            <h1>Registro de Categoria:{nomeDaCategoria}</h1>
+            <h1>Registro de Categoria:{values.nome}</h1>
 
             <form onSubmit={function handleSubmit(infoDoEvento) {
                 infoDoEvento.preventDefault();
                 setCategorias([
                     ...categorias,
-                    nomeDaCategoria
+                    values
                 ]);
+                setValues(valorInicial)
 
             }}>
-                <label>
-                    Nome da Categoria:
+                <div>
+                    <label>
+                        Nome da Categoria:
                     <input
-                        type="text"
-                        value={nomeDaCategoria}
-                        onChange={function errorHandler(infoDoEvento) {
-                            setNomeDaCategoria(infoDoEvento.target.value);
-                        }}
-                    />
-                </label>
+                            type="text"
+                            name="nome"
+                            value={values.nome}
+                            onChange={errorHandler}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Descrição
+                    <textarea
+                            type="text"
+                            name="descrição"
+                            value={values.descrição}
+                            onChange={errorHandler}
+                        />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Cor
+                    <input
+                            type="color"
+                            name="cor"
+                            value={values.cor}
+                            onChange={errorHandler}
+                        />
+                    </label>
+                </div>
                 <button>
                     Registrar
                 </button>
             </form>
 
             <ul>
-                {categorias.map((categoria) => {
+                {categorias.map((categoria, indice) => {
                     return (
-                        <li key={categoria}>
-                            {categoria}
+                        <li key={`${categoria}${indice}`}>
+                            {categoria.nome}
                         </li>
                     )
                 })}
