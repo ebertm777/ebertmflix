bd807ac5998e9c77623807c7fe0a4fadbf46a857
ebertm777 <eber.tmacedo@gmail.com>
2020-08-05 19:04:55 -0300

    back end update

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -29,7 +29,7 @@ function CategoriaRegistro() {
 
     useEffect(() => {
         if (window.location.href.includes('localhost')) {
-            const URL = 'http://localhost:8080/categorias';
+            const URL = 'https://koalaplay.herokuapp.com/categorias';
             fetch(URL)
                 .then(async (respostaDoServer) => {
                     if (respostaDoServer.ok) {
